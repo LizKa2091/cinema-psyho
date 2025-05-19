37dd78da8f058d1ea348c9f19d3808721e6ad060
lizka <lizka4231@gmail.com>
2025-05-20 00:11:48 +0300

    feat: add films list for watch later and disliked films

--- frontend/src/utils/filmsList.ts
+++ frontend/src/utils/filmsList.ts
@@ -1,27 +1,65 @@
-import { IFilmItem } from "../types/film.types";
+import { IFilmsItem } from "../types/film.types";
 
-export const checkFilmStatus = async (filmToCheckId: number) => {
-   const filmsWatchLaterList: string | null = localStorage.getItem('watchLaterList');
-   const filmsDislikedList: string | null = localStorage.getItem('dislikedList');
+type filmAction = 'add' | 'remove';
+
+export const checkFilmStatus = (filmData: IFilmsItem) => {
+   let filmsWatchLaterList: string | null = localStorage.getItem('watchLaterList');
+   let filmsDislikedList: string | null = localStorage.getItem('dislikedList');
 
    let isFilmWatchLater: boolean | null = null;
    let isFilmDisliked: boolean | null = null;
 
    if (!filmsWatchLaterList) {
-      localStorage.setItem('watchLaterList', JSON.stringify({ films: {} }));
+      localStorage.setItem('watchLaterList', JSON.stringify({ films: [] }));
       isFilmWatchLater = false;
    }
 
    if (!filmsDislikedList) {
-      localStorage.setItem('dislikedList', JSON.stringify({ films: {} }));
+      localStorage.setItem('dislikedList', JSON.stringify({ films: [] }));
       isFilmDisliked = false;
    }
 
    if (filmsWatchLaterList && filmsDislikedList) {
-      isFilmWatchLater = JSON.parse(filmsWatchLaterList).films.filter((film: IFilmItem) => filmToCheckId === film.filmId);
-      isFilmDisliked = JSON.parse(filmsDislikedList).films.filter((film: IFilmItem) => filmToCheckId === film.filmId);
+      const watchLaterData = JSON.parse(filmsWatchLaterList);
+      const dislikedData = JSON.parse(filmsDislikedList);
+      
+      isFilmWatchLater = Array.isArray(watchLaterData.films) && watchLaterData.films.some((film: IFilmsItem) => filmData.filmId === film.filmId);
+      isFilmDisliked = Array.isArray(dislikedData.films) && dislikedData.films.some((film: IFilmsItem) => filmData.filmId === film.filmId);
    }
 
-
    return [isFilmWatchLater, isFilmDisliked];
-}
+};
+
+export const filmWatchLaterAction = (filmData: IFilmsItem, action: filmAction) => {
+   const storedData = localStorage.getItem('watchLaterList') || '{"films":[]}';
+   const { films } = JSON.parse(storedData);
+   
+   let updatedFilms;
+   
+   if (action === 'add') {
+     updatedFilms = [...films, filmData];
+   } 
+   else {
+     updatedFilms = films.filter((film: IFilmsItem) => film.filmId !== filmData.filmId);
+   }
+   
+   localStorage.setItem('watchLaterList', JSON.stringify({ films: updatedFilms }));
+   return action === 'add';
+};
+
+export const filmDislikeAction = (filmData: IFilmsItem, action: filmAction) => {
+   const storedData = localStorage.getItem('dislikedList') || '{"films":[]}';
+   const { films } = JSON.parse(storedData);
+   
+   let updatedFilms;
+   
+   if (action === 'add') {
+      updatedFilms = [...films, filmData];
+   } 
+   else {
+      updatedFilms = films.filter((film: IFilmsItem) => film.filmId !== filmData.filmId);
+   }
+   
+   localStorage.setItem('dislikedList', JSON.stringify({ films: updatedFilms }));
+   return action === 'add';
+};
