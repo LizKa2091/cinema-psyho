3c8f22ee7c6a65d9ba6c0c8d7ca630197099f18a
lizka <lizka4231@gmail.com>
2025-05-23 00:13:12 +0300

    feat: add apiresponse type

--- frontend/src/types/profile.types.ts
+++ frontend/src/types/profile.types.ts
@@ -3,3 +3,16 @@ export interface ISavedCategoriesItem {
    label: string;
    icon: any;
 }
+
+export interface IAIResponse {
+   result: string;
+   model: string;
+   usage: {
+      prompt_tokens: number;
+      completion_tokens: number;
+   };
+}
+
+export interface IApiResponse {
+   data: IAIResponse;
+}
