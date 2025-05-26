e09960a6be915050e9bc27d9cfa61ea8d9cef731
lizka <lizka4231@gmail.com>
2025-05-26 22:42:29 +0300

    feat: implement film trailer into film item

--- frontend/src/components/UI/FilmItem.tsx
+++ frontend/src/components/UI/FilmItem.tsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import { Button, Flex } from 'antd';
 import { formatTime } from '../../utils/formatTime';
 import styles from './FilmItem.module.scss';
+import { useFilmTrailer } from '../../hooks/useFilmTrailer';
+import { IFilmTrailerItem } from '../../types/film.types';
+import { YoutubeFilled } from '@ant-design/icons';
 
 interface IFilmItemProps {
    filmId: string;
@@ -11,12 +14,21 @@ interface IFilmItemProps {
 
 const FilmItem: FC<IFilmItemProps> = ({ filmId }) => {
    const [isFactsOpen, setIsFactsOpen] = useState<boolean>(false);
+   const [filteredTrailers, setFilteredTrailers] = useState<IFilmTrailerItem[]>([]);
 
+   const { data: filmTrailers, isError: isTrailerError } = useFilmTrailer(filmId || '');
    const { data, isError, isSuccess } = useFilm(filmId || '');
    
    useEffect(() => {
       setIsFactsOpen(false);
    }, [filmId]);
+
+   useEffect(() => {
+      if (filmTrailers && filmTrailers.items?.length > 0) {
+         const filteredArr = filmTrailers.items.filter((item: IFilmTrailerItem) => item.site === 'YOUTUBE').slice(0, 3);
+         setFilteredTrailers(filteredArr);
+      }
+   }, [filmTrailers])
 
    const stripHtml = (html: string) => {
       const temp = document.createElement('div');
@@ -50,7 +62,20 @@ const FilmItem: FC<IFilmItemProps> = ({ filmId }) => {
                      }
                   </Flex>
                }
-               <img src={data.data.posterUrl} alt={data.data.nameRu} width='100%'/>
+               <img src={data.data.posterUrl} alt={data.data.nameRu} width='100%' />
+               {filmTrailers && filteredTrailers.length > 0 && (
+                  <>
+                     <h2 className={styles.header}>Трейлеры YouTube<YoutubeFilled /></h2>
+                     {filteredTrailers.map((trailer: IFilmTrailerItem, index) => (
+                        <div key={trailer.url} className={styles.videoContainer}>
+                           <iframe src={trailer.url} title={`${trailer.site}${index}`} className={styles.videoFrame} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                        </div>
+                     ))}
+                  </>
+               )}
+               {isTrailerError &&
+                  <span>Возникла ошибка при отображении трейлеров</span>
+               }
             </Flex>
          }
          {isSuccess && !data &&
