e32017f01208b38e9ad1c0c36200b7b93b95effa
lizka <lizka4231@gmail.com>
2025-06-06 20:32:36 +0300

    feat: add films pagination

---
+++ frontend/src/components/UI/FilmsPagination.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { Pagination } from 'antd';
+
+interface IFilmsPaginationProps {
+   totalItems: number;
+   currPage: number;
+   pagesCount: number;
+   handlePageChange: (currPage: number) => void;
+}
+
+const FilmsPagination: FC<IFilmsPaginationProps> = ({ totalItems, currPage, handlePageChange }) => {
+   return (
+      <div>
+         <Pagination align='center' total={totalItems-1} current={currPage} onChange={handlePageChange} pageSize={38} showSizeChanger={false} />
+      </div>
+   )
+}
+
+export default FilmsPagination;
