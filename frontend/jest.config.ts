70421c80f0fd67f60535a91f06593ff37118db64
lizka <lizka4231@gmail.com>
2025-06-17 19:01:25 +0300

    chore: update setup for tests

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,13 +3,19 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
    rootDir: '.',
    preset: 'ts-jest',
-   testEnvironment: 'jsdom',
+   testEnvironment: 'jest-fixed-jsdom',
    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
    moduleNameMapper: {
       '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
       '\\.(jpg|jpeg|png|gif|webp)$' : '<rootDir>/mocks/fileMock.js'
    },
    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+   transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+   },
+   transformIgnorePatterns: [
+      '/node_modules/(?!antd)',
+   ],
 };
 
 export default config;
