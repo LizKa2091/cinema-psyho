e16639c277654c349569093a9828191752e7e265
lizka <lizka4231@gmail.com>
2025-07-03 17:33:20 +0300

    feat(search history): add test

--- frontend/__tests__/searchHistory.test.ts
+++ frontend/__tests__/searchHistory.test.ts
@@ -1,3 +1,4 @@
+import { addItemSearchHistory } from './../src/features/searchHistory';
 import '@testing-library/jest-dom';
 
 describe('search history tests', () => {
@@ -18,4 +19,33 @@ describe('search history tests', () => {
    afterEach(() => {
       jest.restoreAllMocks();
    });
+
+   test('adds new item to search history', () => {
+      const testData = 'test';
+      addItemSearchHistory(testData);
+
+      expect(localStorage.setItem).toHaveBeenCalledWith('searchHistory', expect.any(String));
+
+      const storedData = JSON.parse(mockLocalStorage['searchHistory']);
+
+      expect(storedData.length).toBe(1);
+      expect(storedData[0].label).toBe(testData);
+      expect(typeof storedData[0].timestamp).toBe('number');
+   });
+
+   test('trims history when max storage size is reached', () => {
+      const testData = Array.from({ length: 100 }, (_, i) => ({
+         label: `test ${i}`,
+         timestamp: Date.now()
+      }));
+
+      mockLocalStorage['searchHistory'] = JSON.stringify(testData);
+      addItemSearchHistory('some test');
+
+      const storedData = JSON.parse(mockLocalStorage['searchHistory']);
+
+      expect(storedData.length).toBeLessThanOrEqual(100);
+      expect(storedData.some(item => item.label === 'test 0')).toBe(false);
+      expect(storedData.some(item => item.label === 'some test')).toBe(true);
+   })
 });
