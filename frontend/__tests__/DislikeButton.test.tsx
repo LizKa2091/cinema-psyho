ad3980b6477a255d0e2b8a6556d4a76f740349b6
lizka <lizka4231@gmail.com>
2025-07-09 23:16:16 +0300

    tests(UI buttons): add tests for dislike button

---
+++ frontend/__tests__/DislikeButton.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import DislikeButton from '../src/components/UI/buttons/DislikeButton';
+import * as filmsListUtils from '../src/utils/filmsList';
+
+jest.mock('../src/utils/filmsList', () => ({
+   checkFilmStatus: jest.fn(),
+   filmWatchedAction: jest.fn()
+}));
+
+const mockFilmData = {
+   filmId: 2,
+   nameRu: 'тест',
+   posterUrl: 'url',
+   rating: '2.0',
+   year: '2000',
+   filmLength: '02:20'
+};
+
+describe('DislikeButton tests', () => {
+   beforeEach(() => {
+      jest.clearAllMocks();
+   });
+
+   test('displays suggestive text dont show anymore when film is not hidden', () => {
+      (filmsListUtils.checkFilmStatus as jest.Mock).mockReturnValue([false, false, false]);
+      
+      render(<DislikeButton filmData={mockFilmData} />);
+      
+      const button = screen.getByText('Не показывать больше');
+
+      expect(button).toBeInTheDocument();
+   });
+
+   test('displays suggestive text show again when film is hidden', () => {
+      (filmsListUtils.checkFilmStatus as jest.Mock).mockReturnValue([false, false, true]);
+
+      render(<DislikeButton filmData={mockFilmData} />);
+
+      const button = screen.getByText('Снова показывать');
+
+      expect(button).toBeInTheDocument();
+   });
+
+   test('toggles hidden status on click', () => {
+      (filmsListUtils.checkFilmStatus as jest.Mock).mockReturnValue([false, false, false]);
+
+      const filmWatchedActionMock = filmsListUtils.filmWatchedAction as jest.Mock;
+
+      render(<DislikeButton filmData={mockFilmData} />);
+
+      const button = screen.getByText('Не показывать больше');
+
+      fireEvent.click(button);
+
+      expect(filmWatchedActionMock).toHaveBeenCalledWith(mockFilmData, 'add');
+      
+
+      const updatedButton = screen.getByText('Снова показывать');
+
+      expect(updatedButton).toBeInTheDocument();
+   });
+});
